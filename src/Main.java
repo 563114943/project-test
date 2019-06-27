7251a8f350db574d71066f0e598c48e4b2ef689b
563114943 <563114943@qq.com>
2019-06-27 16:26:02 +0800

    project-test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String adds="111";
+        System.out.println("Hello World!"+adds);
+
     }
 }
